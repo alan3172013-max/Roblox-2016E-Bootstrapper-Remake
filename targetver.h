6844c44b47d0e477aededd6d04bee8d23086c8fd
triple_t <alan3172013@gmail.com>
2026-03-13 16:26:22 +0800

    Add targetver.h to define Windows Vista as target platform for Visual Studio 2012

---
+++ targetver.h
@@ -0,0 +1,10 @@
+// targetver.h
+#pragma once
+
+// Define the Windows Vista target platform for Visual Studio 2012.
+#include <SDKDDKVer.h>
+
+// Change this if your target Windows platform is not Windows Vista
+// See WinBase.h for the definition of WINVER
+#define WINVER 0x0600 // Windows Vista
+#define _WIN32_WINNT 0x0600 // Windows Vista
